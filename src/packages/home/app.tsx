8fb7e33167250e766ab961ea8ce75248e8b99e41
sqleo <sqliuen@gmail.com>
2022-09-19 09:44:05 +0800

    ts 配置

--- src/packages/home/app.tsx
+++ src/packages/home/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import a from "@src/containers/assets/images/images.png"
+import a from  "@src/containers/assets/images/images.png"
 import "./index.scss"
 
 const element = <div>asd
